d9e6bf276f464b15a8c7abd73aa53e5abea75248
sharpiro <dsharpbb09@gmail.com>
2015-09-27 19:08:27 -0400

    Cleanup

--- src/Health.Core/Entities/Meal.cs
+++ src/Health.Core/Entities/Meal.cs
@@ -3,13 +3,6 @@ using System.Collections.Generic;
 
 namespace Health.Core.Entities
 {
-    //public class Meal
-    //{
-    //    public int Id { get; set; }
-    //    public int MealNumber { get; set; }
-    //    public DateTime DayId { get; set; }
-    //    public virtual Day Day { get; set; }
-    //}
     public class Meal
     {
         public int Id { get; set; }
