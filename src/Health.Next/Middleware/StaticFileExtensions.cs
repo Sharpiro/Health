0d3c7a85aca1c8002fd6b667a353880f9627ac0a
sharpiro <dsharpbb09@gmail.com>
2017-04-08 11:45:05 -0400

    client angular code now functional in VS and CLI

---
+++ src/Health.Next/Middleware/StaticFileExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class StaticFileExtensions
+    {
+        public static IApplicationBuilder UseOtherDirectory(this IApplicationBuilder app, IHostingEnvironment env, string otherDirectory)
+        {
+            var path = Path.Combine(env.ContentRootPath, otherDirectory);
+            var options = new StaticFileOptions { FileProvider = new PhysicalFileProvider(path), RequestPath = new PathString($"/{otherDirectory}") };
+            app.UseStaticFiles(options);
+            return app;
+        }
+    }
+}
