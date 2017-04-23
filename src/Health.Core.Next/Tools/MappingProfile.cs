3145f75dac1d58129652f605dbeee47a58fe0cf8
sharpiro <dsharpbb09@gmail.com>
2017-04-23 14:43:48 -0400

    fixed fatal IIS issue added automapper and food service

--- src/Health.Core.Next/Tools/MappingProfile.cs
+++ src/Health.Core.Next/Tools/MappingProfile.cs
@@ -1,14 +1,14 @@
-﻿//using AutoMapper;
-//using Health.Core.Next.DataAccess.Entities;
-//using Health.Core.Next.Models;
+﻿using AutoMapper;
+using Health.Core.Next.DataAccess.Entities;
+using Health.Core.Next.Models;
 
-//namespace Health.Core.Next.Tools
-//{
-//    public class MappingProfile : Profile
-//    {
-//        public MappingProfile()
-//        {
-//            CreateMap<Food, FoodDto>();
-//        }
-//    }
-//}
+namespace Health.Core.Next.Tools
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Food, FoodDto>();
+        }
+    }
+}
--- src/Health.Next/Startup.cs
+++ src/Health.Next/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Health.Core.Next.DataAccess;
+﻿using AutoMapper;
+using Health.Core.Next.DataAccess;
 using Health.Core.Next.Services;
+using Health.Core.Next.Tools;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -13,46 +15,47 @@ namespace Health.Next
 {
     public class Startup
     {
-        //private readonly IConfiguration _configuration;
+        private readonly IConfiguration _configuration;
 
-        public Startup()
+        public Startup(IHostingEnvironment env)
         {
-            //var builder = new ConfigurationBuilder()
-            //.SetBasePath(env.ContentRootPath)
-            //.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-            //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-            //.AddEnvironmentVariables();
-            //_configuration = builder.Build();
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables();
+            _configuration = builder.Build();
         }
 
-        public void ConfigureServices()
+        public void ConfigureServices(IServiceCollection services)
         {
-            ////services.AddTransient(factory =>
-            ////    new MapperConfiguration(cfg =>
-            ////        cfg.AddProfile<MappingProfile>()).CreateMapper()
-            ////);
-            //services.AddMvc();
-            //services.AddScoped<HealthContext>();
-            //services.AddScoped<HealthService>();
-            //services.AddDbContext<HealthContext>(options =>
-            //    options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
-            //services.AddIdentity<IdentityUser, IdentityRole>()
-            //    .AddEntityFrameworkStores<HealthContext>()
-            //    .AddDefaultTokenProviders();
+            services.AddTransient(factory =>
+                new MapperConfiguration(cfg =>
+                    cfg.AddProfile<MappingProfile>()).CreateMapper()
+            );
+            services.AddMvc();
+            services.AddScoped<HealthContext>();
+            services.AddScoped<HealthService>();
+            services.AddScoped<FoodService>();
+            services.AddDbContext<HealthContext>(options =>
+                options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<HealthContext>()
+                .AddDefaultTokenProviders();
         }
 
-        public void Configure()
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            //loggerFactory.AddConsole();
+            loggerFactory.AddConsole();
 
-            //if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
-            ////app.UseOtherDirectory(env, "node_modules");
-            //app.UseMvc(builder =>
-            //{
-            //    builder.MapRoute(name: "defaultApi", template: "api/{controller}/{action}/{id?}");
-            //});
-            //app.UseFileServer();
+            //app.UseOtherDirectory(env, "node_modules");
+            app.UseMvc(builder =>
+            {
+                builder.MapRoute(name: "defaultApi", template: "api/{controller}/{action}/{id?}");
+            });
+            app.UseFileServer();
         }
 
         public static void Main(string[] args)
