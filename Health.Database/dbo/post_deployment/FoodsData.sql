49510cbc37f57c9b78e404cf07e4daa5e309d136
sharpiro <dsharpbb09@gmail.com>
2017-05-17 19:23:53 -0400

    updated post deployment foods

--- Health.Database/dbo/post_deployment/FoodsData.sql
+++ Health.Database/dbo/post_deployment/FoodsData.sql
@@ -3,14 +3,14 @@
 MERGE INTO [dbo].[Foods] AS TARGET
 USING (VALUES 
 	(6, 120, N'Chicken', 4, 0, 1, 0, 0, 26, N'oz', 50, 0, 1),
-	(7, 200, N'Eggs', 1, 0, 10, 0, 75, 21, N'custom', 272, 0, 1),
+	(7, 120, N'Eggs', 1, 0, 5, 0, 150, 16, N'custom', 230, 0, 1),
 	(8, 100, N'Almonds', 18, 1, 9, 2, 130, 4, N'grams', 0, 1, 1),
 	(9, 210, N'Oatmeal', 2, 32, 4, 6, 210, 8, N'item', 150, 0, 1),
 	(11, 40, N'Apple', 1, 17, 0, 5, 0, 0, N'item', 0, 17, 1),
 	(12, 35, N'Soup', 1, 8, 0, 4, 580, 3, N'half-can', 470, 2, 1),
 	(13, 180, N'Protein Bar', 1, 4, 8, 13, 140, 20, N'item', 170, 1, 1),
 	(14, 110, N'Beans', 130, 14, 1, 6, 400, 6, N'grams', 530, 2, 0),
-	(16, 150, N'Potatoes', 213, 34, 0, 4, 969, 4, N'grams', 38, 3, 1),
+	(16, 110, N'Potatoes', 148, 26, 0, 2, 620, 3, N'grams', 0, 1, 1),
 	(17, 130, N'Cheeto-Crunch', 28, 20, 5, 0, 60, 2, N'grams', 230, 0, 0),
 	(18, 150, N'Cheeto-Puff', 28, 16, 9, 0, 0, 2, N'grams', 290, 1, 0),
 	(19, 80, N'Yasso-Pop', 1, 16, 2, 1, 0, 5, N'item', 40, 13, 1),
@@ -19,7 +19,11 @@ USING (VALUES
 	(22, 360, N'Strudles', 2, 54, 14, 1, 0, 4, N'item', 360, 20, 0),
 	(23, 80, N'Banana', 1, 27, 0, 3, 422, 1, N'item', 1, 14, 1),
 	(24, 100, N'Cheerios', 28, 20, 2, 3, 180, 3, N'grams', 140, 1, 0),
-	(25, 160, N'Yogurt', 300, 12, 0, 0, 320, 29, N'grams', 107, 12, 1)
+	(25, 120, N'Yogurt', 225, 9, 0, 0, 250, 22, N'grams', 90, 9, 1),
+	(26, 190, N'Almond Butter', 32, 6, 18, 3, 0, 7, N'grams', 0, 1, 1),
+	(27, 114, N'Sweet Potato', 133, 27, 0, 4, 448, 2, N'grams', 73, 6, 1),
+	(28, 60, N'Wheat Bread', 1, 11, 0, 2, 0, 4, N'item', 0, 1, 1)
+
 
 ) AS SOURCE 
 ([Id], [Calories], [Name], [ServingSize], [Carbs], [Fat], [Fiber], [Potassium], [Protein], [ServingName], [Sodium], [Sugar], [IsActive])
